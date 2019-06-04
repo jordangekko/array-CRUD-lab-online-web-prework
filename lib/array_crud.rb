b877b23234cf6dcbc2104809917758abd2371963
KRIS DAVIS <krissdavis18@mail.com>
2019-06-04 01:28:17 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,34 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  flatiron_school = ["ruby", "HTML", "CSS", "javascript"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  flatiron_school.push("loops")
 
 def add_element_to_start_of_array(array, element)
-  
+  flatiron_school.unshift("booleans")
 end
 
 def remove_element_from_end_of_array(array)
-  
+flatiron_school_loops = flatiron_school.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  flatiron_school_booleans = flatiron_school.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  flatiron_school.index
 end
 
 def retrieve_first_element_from_array(array)
-  
+  flatiron_school [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  flatiron_school [3]
 end
